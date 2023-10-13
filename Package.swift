677b22b558c24a3462a71fa5efb156aca0896e81
Dominik Liehr <dominik_liehr@me.com>
2023-10-13 22:36:42 +0200

    Init. Added dependencies

--- Package.swift
+++ Package.swift
@@ -5,15 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "EnergyPackage",
+	platforms: [
+		.macOS(.v12), .iOS(.v15)
+	],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
+		.library(
             name: "EnergyPackage",
             targets: ["EnergyPackage"]),
     ],
+	dependencies: [
+		.package(url: "https://github.com/sroebert/mqtt-nio", exact: .init(2, 8, 1)),
+		.package(url: "https://github.com/influxdata/influxdb-client-swift", exact: .init(1, 6, 0))
+	],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "EnergyPackage"),
         .testTarget(
